60b930ab8db6cbb56a47597a72c579f5b4e16404
Carlos Sanchez <papachoco@gmail.com>
2017-07-21 23:35:02 -0500

    add evolve3 to remove transaction records for deleted / missing objects

---
+++ src/nti/app/recorder/generations/evolve3.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+generation = 3
+
+from zope import component
+from zope import interface
+
+from zope.component.hooks import site
+from zope.component.hooks import setHooks
+
+from zope.intid.interfaces import IIntIds
+
+from nti.dataserver.interfaces import IDataserver
+from nti.dataserver.interfaces import IOIDResolver
+
+from nti.recorder.index import IX_RECORDABLE
+
+from nti.recorder.index import get_transaction_catalog
+
+from nti.recorder.interfaces import ITransactionRecord
+
+
+@interface.implementer(IDataserver)
+class MockDataserver(object):
+
+    root = None
+
+    def get_by_oid(self, oid, ignore_creator=False):
+        resolver = component.queryUtility(IOIDResolver)
+        if resolver is None:
+            logger.warn("Using dataserver without a proper ISiteManager.")
+        else:
+            return resolver.get_object_by_oid(oid, ignore_creator=ignore_creator)
+        return None
+
+
+def do_evolve(context):
+    setHooks()
+    conn = context.connection
+    root = conn.root()
+    ds_folder = root['nti.dataserver']
+
+    count = 0
+    mock_ds = MockDataserver()
+    mock_ds.root = ds_folder
+    component.provideUtility(mock_ds, IDataserver)
+
+    with site(ds_folder):
+        assert component.getSiteManager() == ds_folder.getSiteManager(), \
+               "Hooks not installed?"
+
+        lsm = ds_folder.getSiteManager()
+        intids = lsm.getUtility(IIntIds)
+        catalog = get_transaction_catalog(lsm)
+        index = catalog[IX_RECORDABLE]
+        for rec_id, trxs in list(index.values_to_documents.items()):
+            recordable = intids.queryObject(rec_id)
+            if recordable is None:
+                count += 1
+                for trx_id in tuple(trxs):
+                    transaction = intids.queryObject(trx_id)
+                    try:
+                        intids.force_unregister(trx_id, transaction)
+                    except KeyError:
+                        pass
+                    catalog.unindex_doc(trx_id)
+                    if ITransactionRecord.providedBy(transaction):
+                        transaction.__parent__ = None
+
+    component.getGlobalSiteManager().unregisterUtility(mock_ds, IDataserver)
+    logger.info('Evolution %s done. %s record(s) unregistered', 
+                generation, count)
+    return count
+
+
+def evolve(context):
+    """
+    Evolve to gen 3 by unregistering leaked transaction records
+    """
+    do_evolve(context)
---
+++ src/nti/app/recorder/generations/tests/test_evolve3.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
+
+from hamcrest import is_
+from hamcrest import none
+from hamcrest import assert_that
+
+import fudge
+import unittest
+
+from zope import component
+
+from zope.intid.interfaces import IIntIds
+
+from zc.catalog.index import ValueIndex
+
+from nti.app.recorder.generations import evolve3
+
+from nti.recorder.index import IX_RECORDABLE
+from nti.recorder.index import get_transaction_catalog
+
+from nti.recorder.record import TransactionRecord
+
+from nti.app.recorder.tests import SharedConfiguringTestLayer
+
+from nti.dataserver.tests import mock_dataserver
+
+
+class TestEvolve3(unittest.TestCase):
+
+    layer = SharedConfiguringTestLayer
+
+    @mock_dataserver.WithMockDSTrans
+    def test_installed(self):
+        conn = mock_dataserver.current_transaction
+        record = TransactionRecord()
+        record.username = u'ichigo'
+        conn.add(record)
+        intids = component.getUtility(IIntIds)
+        trx_id = intids.register(record)
+
+        # index
+        generated = intids.generateId(None)
+        catalog = get_transaction_catalog()
+        index = catalog[IX_RECORDABLE]
+        ValueIndex.index_doc(index, trx_id, generated)
+        
+        context = fudge.Fake().has_attr( connection=conn )
+        count = evolve3.do_evolve(context)
+        assert_that(count, is_(1))
+        assert_that(intids.queryId(record), is_(none()))
