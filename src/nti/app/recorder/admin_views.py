1c9b973865e6b6a73ba6a5256a4022a4b3884167
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-09-13 12:37:44 -0500

    minor

--- src/nti/app/recorder/admin_views.py
+++ src/nti/app/recorder/admin_views.py
@@ -8,12 +8,12 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
-import six
-
 from requests.structures import CaseInsensitiveDict
 
 from pyramid.view import view_config
 from pyramid.view import view_defaults
+
+import six
 
 from zope import component
 from zope import lifecycleevent
